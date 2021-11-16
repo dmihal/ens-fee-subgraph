3c3bb7b65096f77471f5a16b533c995510f9bb78
David Mihal <david@dmihal.com>
2021-11-16 14:04:41 +0000

    Use UniV1 for ETH price

--- src/ethRegistrar.ts
+++ src/ethRegistrar.ts
@@ -1,22 +1,15 @@
 // Import types and APIs from graph-ts
 import { BigInt, BigDecimal, Address } from '@graphprotocol/graph-ts'
-
-import {
-  createEventID, ROOT_NODE, EMPTY_ADDRESS,
-  uint256ToByteArray, byteArrayFromHex, concat
-} from './utils'
-
-import { Chainlink } from "./types/EthRegistrarController/Chainlink"
-
 import {
   NameRegistered as ControllerNameRegisteredEvent,
   NameRenewed as ControllerNameRenewedEvent
 } from './types/EthRegistrarController/EthRegistrarController'
-
+import { EthPurchase, TokenPurchase } from './types/UniswapV1Pool/UniswapV1Pool'
 // Import entity types generated from the GraphQL schema
 import { ENS } from './types/schema'
 
 let EIGHT_DECIMALS = BigInt.fromI32(10).pow(8).toBigDecimal()
+let TWELVE_DECIMALS = BigInt.fromI32(10).pow(12).toBigDecimal()
 let EIGHTEEN_DECIMALS = BigInt.fromI32(10).pow(18).toBigDecimal()
 
 function getSingleton(): ENS {
@@ -25,20 +18,16 @@ function getSingleton(): ENS {
     ens = new ENS('ens')
     ens.ethCollected = BigInt.fromI32(0).toBigDecimal()
     ens.usdCollected = BigInt.fromI32(0).toBigDecimal()
+    ens.ethPrice = BigInt.fromI32(0).toBigDecimal()
   }
   return ens!
-}
-
-function getETHPrice(): BigDecimal {
-  let chainlink = Chainlink.bind(Address.fromString('0x5f4ec3df9cbd43714fe2740f5e3616155c5b8419'))
-  return chainlink.latestAnswer().divDecimal(EIGHT_DECIMALS)
 }
 
 export function handleNameRegisteredByController(event: ControllerNameRegisteredEvent): void {
   let ens = getSingleton()
   let cost = event.params.cost.divDecimal(EIGHTEEN_DECIMALS)
   ens.ethCollected += cost
-  // ens.usdCollected += cost * getETHPrice()
+  ens.usdCollected += cost * ens.ethPrice
   ens.save()
 }
 
@@ -46,6 +35,24 @@ export function handleNameRenewedByController(event: ControllerNameRenewedEvent)
   let ens = getSingleton()
   let cost = event.params.cost.divDecimal(EIGHTEEN_DECIMALS)
   ens.ethCollected += cost
-  // ens.usdCollected += cost * getETHPrice()
+  ens.usdCollected += cost * ens.ethPrice
+  ens.save()
+}
+
+export function handleETHPurchase(event: EthPurchase): void {
+  let ens = getSingleton()
+  ens.ethPrice = event.params.tokens_sold
+    .divDecimal(event.params.eth_bought.toBigDecimal())
+    .times(TWELVE_DECIMALS)
+
+  ens.save()
+}
+
+export function handleUSDCPurchase(event: TokenPurchase): void {
+  let ens = getSingleton()
+  ens.ethPrice = event.params.tokens_bought
+    .divDecimal(event.params.eth_sold.toBigDecimal())
+    .times(TWELVE_DECIMALS)
+
   ens.save()
 }
