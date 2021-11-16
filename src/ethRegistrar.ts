8066c744328a328f0926866c2556beae1b9cb03e
David Mihal <david@dmihal.com>
2021-11-16 13:40:39 +0000

    subgraph for tracking ETH fees

--- src/ethRegistrar.ts
+++ src/ethRegistrar.ts
@@ -1,21 +1,12 @@
 // Import types and APIs from graph-ts
-import {
-  ByteArray,
-  crypto,
-  ens
-} from '@graphprotocol/graph-ts'
+import { BigInt, BigDecimal, Address } from '@graphprotocol/graph-ts'
 
 import {
   createEventID, ROOT_NODE, EMPTY_ADDRESS,
   uint256ToByteArray, byteArrayFromHex, concat
 } from './utils'
 
-// Import event types from the registry contract ABI
-import {
-  NameRegistered as NameRegisteredEvent,
-  NameRenewed as NameRenewedEvent,
-  Transfer as TransferEvent,
-} from './types/BaseRegistrar/BaseRegistrar'
+import { Chainlink } from "./types/EthRegistrarController/Chainlink"
 
 import {
   NameRegistered as ControllerNameRegisteredEvent,
@@ -23,95 +14,38 @@ import {
 } from './types/EthRegistrarController/EthRegistrarController'
 
 // Import entity types generated from the GraphQL schema
-import { Account, Domain, Registration, NameRegistered, NameRenewed, NameTransferred } from './types/schema'
+import { ENS } from './types/schema'
 
-var rootNode:ByteArray = byteArrayFromHex("93cdeb708b7545dc668eb9280176169d1c33cfd8ed6f04690a0bcc88a93fc4ae")
+let EIGHT_DECIMALS = BigInt.fromI32(10).pow(8).toBigDecimal()
+let EIGHTEEN_DECIMALS = BigInt.fromI32(10).pow(18).toBigDecimal()
 
-export function handleNameRegistered(event: NameRegisteredEvent): void {
-  let account = new Account(event.params.owner.toHex())
-  account.save()
-
-  let label = uint256ToByteArray(event.params.id)
-  let registration = new Registration(label.toHex())
-  registration.domain = crypto.keccak256(concat(rootNode, label)).toHex()
-  registration.registrationDate = event.block.timestamp
-  registration.expiryDate = event.params.expires
-  registration.registrant = account.id
-
-  let labelName = ens.nameByHash(label.toHexString())
-  if (labelName != null) {
-    registration.labelName = labelName
+function getSingleton(): ENS {
+  let ens = ENS.load('ens')
+  if (!ens) {
+    ens = new ENS('ens')
+    ens.ethCollected = BigInt.fromI32(0).toBigDecimal()
+    ens.usdCollected = BigInt.fromI32(0).toBigDecimal()
   }
-  registration.save()
+  return ens!
+}
 
-  let registrationEvent = new NameRegistered(createEventID(event))
-  registrationEvent.registration = registration.id
-  registrationEvent.blockNumber = event.block.number.toI32()
-  registrationEvent.transactionID = event.transaction.hash
-  registrationEvent.registrant = account.id
-  registrationEvent.expiryDate = event.params.expires
-  registrationEvent.save()
+function getETHPrice(): BigDecimal {
+  let chainlink = Chainlink.bind(Address.fromString('0x5f4ec3df9cbd43714fe2740f5e3616155c5b8419'))
+  return chainlink.latestAnswer().divDecimal(EIGHT_DECIMALS)
 }
 
 export function handleNameRegisteredByController(event: ControllerNameRegisteredEvent): void {
-  let domain = new Domain(crypto.keccak256(concat(rootNode, event.params.label)).toHex())
-  if(domain.labelName !== event.params.name) {
-    domain.labelName = event.params.name
-    domain.name = event.params.name + '.eth'
-    domain.save()
-  }
-
-  let registration = Registration.load(event.params.label.toHex());
-  if(registration == null) return
-  registration.labelName = event.params.name
-  registration.cost = event.params.cost
-  registration.save()
+  let ens = getSingleton()
+  let cost = event.params.cost.divDecimal(EIGHTEEN_DECIMALS)
+  ens.ethCollected += cost
+  // ens.usdCollected += cost * getETHPrice()
+  ens.save()
 }
 
 export function handleNameRenewedByController(event: ControllerNameRenewedEvent): void {
-  let domain = new Domain(crypto.keccak256(concat(rootNode, event.params.label)).toHex())
-  if(domain.labelName !== event.params.name) {
-    domain.labelName = event.params.name
-    domain.name = event.params.name + '.eth'
-    domain.save()
-  }
-
-  let registration = Registration.load(event.params.label.toHex());
-  if(registration == null) return
-  registration.labelName = event.params.name
-  registration.cost = event.params.cost
-  registration.save()
-}
-
-export function handleNameRenewed(event: NameRenewedEvent): void {
-  let label = uint256ToByteArray(event.params.id)
-  let registration = new Registration(label.toHex())
-  registration.expiryDate = event.params.expires
-  registration.save()
-
-  let registrationEvent = new NameRenewed(createEventID(event))
-  registrationEvent.registration = registration.id
-  registrationEvent.blockNumber = event.block.number.toI32()
-  registrationEvent.transactionID = event.transaction.hash
-  registrationEvent.expiryDate = event.params.expires
-  registrationEvent.save()
-}
-
-export function handleNameTransferred(event: TransferEvent): void {
-  let account = new Account(event.params.to.toHex())
-  account.save()
-
-  let label = uint256ToByteArray(event.params.tokenId)
-  let registration = Registration.load(label.toHex())
-  if(registration == null) return;
-
-  registration.registrant = account.id
-  registration.save()
-
-  let transferEvent = new NameTransferred(createEventID(event))
-  transferEvent.registration = label.toHex()
-  transferEvent.blockNumber = event.block.number.toI32()
-  transferEvent.transactionID = event.transaction.hash
-  transferEvent.newOwner = account.id
-  transferEvent.save()
+  let ens = getSingleton()
+  let cost = event.params.cost.divDecimal(EIGHTEEN_DECIMALS)
+  ens.ethCollected += cost
+  // ens.usdCollected += cost * getETHPrice()
+  ens.save()
 }
